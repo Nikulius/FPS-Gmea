719a445ae5d0f4c8a96834ef865c6ac1875c0bc8
Nikolas Boundy <niboundy@plankton.emp.sil>
2020-11-30 17:53:45 +0200

    Initial commit

---
+++ Assets/Script/Player scripts/PlayerJump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float jumpForce;
+    [SerializeField] Transform groundChecker;
+    [SerializeField] float checkRadius;
+    [SerializeField] LayerMask groundLayer;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround()) {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+    bool IsOnGround() {
+        Collider[] colliders = Physics.OverlapSphere(groundChecker.position, checkRadius, groundLayer);
+
+        if (colliders.Length > 0) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
---
+++ Assets/Script/Player scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{   
+    private Rigidbody rb ;
+    [SerializeField] float sprintMultiplier = 1.5f;
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public float speed;
+
+    void Update() 
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = transform.right * x + transform.forward * z;
+
+        float actualSpeed = speed;
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            actualSpeed *= sprintMultiplier;
+        }
+        
+        rb.MovePosition(transform.position + movement.normalized * actualSpeed * Time.deltaTime);
+    }
+}
